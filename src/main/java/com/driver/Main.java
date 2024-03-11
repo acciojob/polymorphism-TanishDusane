df860c9d535d31c4b76f9235e2dbfb637cb2b5c6
Tanish Dusane <91111180+DeathxHunter@users.noreply.github.com>
2024-03-11 17:21:16 +0530

    Polymorphism-Java-Assignment-Changed the expression

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,7 +9,7 @@ public class Main {
         // Call the overloaded methods of the Product class and store the results
         int ans1 = p.product(10,20); // Calls the product method that takes two integers
         int ans2 = p.product(10,20,30); // Calls the product method that takes three integers
-        double ans3 = p.product(40.0,15.0); // Calls the product method that takes two doubles
+        double ans3 = p.product(40.0,3.0); // Calls the product method that takes two doubles
 
         // Print the results
         System.out.println(ans1 + "\t" + ans2 + "\t" + ans3);
@@ -21,14 +21,14 @@ public class Main {
                 return (x*y);
             }
 
-            // Method to calculate the sum of three integers
+            // Method to calculate the product of three integers
             public int product(int x, int y, int z) {
-                return (x+y+z);
+                return (x*y*z);
             }
 
-            // Method to calculate the division of two doubles
+            // Method to calculate the product of two doubles
             public double product(double x, double y) {
-                return (x/y);
+                return (x*y);
             }
         }
     }
